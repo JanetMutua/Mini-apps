2f089e71579108766278288c638670da68d88c7d
Janet <janetmutua04@gmail.com>
2023-04-07 08:48:59 +0300

    Intents

--- DictionApp/app/src/main/java/com/example/dictionapp/LetterAdapter.kt
+++ DictionApp/app/src/main/java/com/example/dictionapp/LetterAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.dictionapp
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -31,5 +33,7 @@ class LetterAdapter: RecyclerView.Adapter<LetterAdapter.LetterViewHolder>() {
     override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
         val letter = letterList[position]
         holder.button.text = letter.toString()
+
+
     }
 }
