bf2c676405cdea52810a8f3de1fcce913d57da61
Janet <janetmutua04@gmail.com>
2023-05-07 07:22:37 +0300

    Feat: Setup Bottom navigation with a Floating Action Bar - XML

--- BottomBar/app/src/main/java/com/example/bottombar/MainActivity.kt
+++ BottomBar/app/src/main/java/com/example/bottombar/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.bottombar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bottombar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //removing the shadow effect on the BottomNavigationView
+        binding.bottomNavigationView.background = null
+
+        //making the placeholder menu item unclickable
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
     }
 }
