7206188f40c33f2f26465fbcbf36fba860d17f95
Janet <janetmutua04@gmail.com>
2023-05-15 21:07:41 +0300

    implement modifiers

--- NumeroUno/app/src/main/java/com/example/numerouno/MainActivity.kt
+++ NumeroUno/app/src/main/java/com/example/numerouno/MainActivity.kt
@@ -3,18 +3,20 @@ package com.example.numerouno
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.numerouno.ui.theme.NumeroUnoTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,19 +25,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             NumeroUnoTheme {
                 // A surface container using the 'background' color from the theme
-                Column(
-                    modifier = Modifier
-                        .background(color = Color.Cyan)
-                        .fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    Text(text = "Hello Janet")
-                    Text(text = "Ho are you today")
-                }
 
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyCardView(
+    painter: Painter,
+    contentDescription:String,
+    title:String,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp)
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+
+        }
+    }
+}
