7ec19a123727a1bfa580466a12879b3742b248fa
Janet <janetmutua04@gmail.com>
2023-05-29 20:11:27 +0300

    feat updates

--- NumeroUno/app/src/main/java/com/example/numerouno/MainActivity.kt
+++ NumeroUno/app/src/main/java/com/example/numerouno/MainActivity.kt
@@ -12,44 +12,87 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.numerouno.ui.theme.NumeroUnoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NumeroUnoTheme {
-                // A surface container using the 'background' color from the theme
+            val painter = painterResource(id = R.drawable.vb)
+            val title = "So good they can't ignore you"
+            val contentDescription = "Image of an apartment with glass balconies."
 
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(20.dp)
+
+            ){
+                ImageCardWithText(
+                    title = title,
+                    painter = painter,
+                    contentDescription = contentDescription
+                )
             }
+
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyCardView(
+fun ImageCardWithText(
+    title:String,
     painter: Painter,
     contentDescription:String,
-    title:String,
-    modifier: Modifier = Modifier
+    modifier:Modifier = Modifier
 ){
     Card(
         modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp)
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
+        shape = RoundedCornerShape(18.dp),
+        elevation = CardDefaults.cardElevation(8.dp)
+    ){
+        Box(modifier = Modifier.height(200.dp)){
             Image(
                 painter = painter,
                 contentDescription = contentDescription,
                 contentScale = ContentScale.Crop
             )
 
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+
+                        )
+                    )
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ){
+                Text(title, style = TextStyle(color = Color.White, fontSize = 18.sp))
+            }
+
         }
     }
 }
+
