f870698a6cdadb9229fb7430bd0f5d2ff79ad830
Janet <janetmutua04@gmail.com>
2023-06-04 20:37:35 +0300

    snackbars

--- NumeroUno/app/src/main/java/com/example/numerouno/lessons/SnackbarsAndInputs.kt
+++ NumeroUno/app/src/main/java/com/example/numerouno/lessons/SnackbarsAndInputs.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GreetingCard(){
 
